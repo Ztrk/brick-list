f9b176be0e3c8e87e716cc2258072002c4705087
Marcin Zatorski <mar3211233@gmail.com>
2020-05-21 13:36:37 +0200

    Add preference screen

--- app/src/main/java/com/example/bricklist/MainActivity.kt
+++ app/src/main/java/com/example/bricklist/MainActivity.kt
@@ -87,7 +87,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when(item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
