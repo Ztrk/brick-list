879f939c3de1e10d6b33f4659350472127169473
Marcin Zatorski <mar3211233@gmail.com>
2020-05-23 17:02:26 +0200

    Improve messages when adding new project

--- app/src/main/java/com/example/bricklist/NewInventoryActivity.kt
+++ app/src/main/java/com/example/bricklist/NewInventoryActivity.kt
@@ -1,11 +1,16 @@
 package com.example.bricklist
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_new_inventory.*
 import kotlinx.android.synthetic.main.content_new_inventory.*
+import kotlin.math.min
+import kotlin.text.StringBuilder
 
 class NewInventoryActivity : AppCompatActivity() {
     private lateinit var viewModel: NewInventoryViewModel
@@ -17,10 +22,6 @@ class NewInventoryActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(NewInventoryViewModel::class.java)
 
-        checkButton.setOnClickListener { _ ->
-
-        }
-
         addButton.setOnClickListener {
             val setNumber = setNumberEdit.text
             val name = nameEdit.text.toString()
@@ -28,8 +29,61 @@ class NewInventoryActivity : AppCompatActivity() {
         }
 
         viewModel.result.observe(this, Observer {
-            resultText.text = it
+            spinner.visibility =
+                if (it == NewInventoryViewModel.State.IN_PROGRESS)
+                    View.VISIBLE
+                else
+                    View.GONE
+
+            when (it) {
+                NewInventoryViewModel.State.WRONG_URL -> {
+                    Snackbar.make(coordinatorLayout, R.string.add_wrong_url, Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+                NewInventoryViewModel.State.NETWORK_ERROR -> {
+                    Snackbar.make(coordinatorLayout, R.string.add_network_error, Snackbar.LENGTH_SHORT)
+                        .show()
+                }
+                NewInventoryViewModel.State.SUCCESS -> {
+                    Toast.makeText(this, R.string.add_success, Toast.LENGTH_SHORT).show()
+                }
+                else -> {}
+            }
+
+            if (it != NewInventoryViewModel.State.NONE &&
+                it != NewInventoryViewModel.State.IN_PROGRESS) {
+                viewModel.resetResult()
+            }
         })
 
+        viewModel.bricksNotFound.observe(this, Observer {
+            if (it.isNotEmpty()) {
+                val text = StringBuilder(getString(R.string.add_bricks_not_found))
+                text.append('\n')
+
+                val maxVisible = min(3, it.count())
+                for (code in it.subList(0, maxVisible)) {
+                    text.append(code)
+                    text.append('\n')
+                }
+                if (it.count() > maxVisible) {
+                    text.append(getString(R.string.add_bricks_not_found_others,
+                        it.count() - maxVisible))
+                }
+
+                Toast.makeText(this, text.toString(), Toast.LENGTH_LONG)
+                    .show()
+
+                viewModel.resetBricksNotFound()
+            }
+        })
+
+        viewModel.addWhenInProgress.observe(this, Observer {
+            if (it) {
+                Snackbar.make(coordinatorLayout, R.string.add_already_in_progress,
+                    Snackbar.LENGTH_SHORT).show()
+                viewModel.handledAddWhenInProgress()
+            }
+        })
     }
 }
--- app/src/main/java/com/example/bricklist/NewInventoryViewModel.kt
+++ app/src/main/java/com/example/bricklist/NewInventoryViewModel.kt
@@ -6,10 +6,13 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.preference.PreferenceManager
+import com.android.volley.ClientError
 import com.android.volley.VolleyError
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 
 class NewInventoryViewModel(application: Application) : AndroidViewModel(application) {
@@ -22,17 +25,53 @@ class NewInventoryViewModel(application: Application) : AndroidViewModel(applica
     private val urlPrefix = PreferenceManager.getDefaultSharedPreferences(application)
         .getString("url_prefix", "http://fcds.cs.put.poznan.pl/MyWeb/BL/")
 
-    private val _result = MutableLiveData("")
-    val result: LiveData<String>
+    private val _result = MutableLiveData(State.NONE)
+    val result: LiveData<State>
         get() = _result
 
+    fun resetResult() {
+        if (_result.value != State.IN_PROGRESS) {
+            _result.value = State.NONE
+        }
+    }
+
+    private val _bricksNotFound = MutableLiveData(listOf<String>())
+    val bricksNotFound: LiveData<List<String>>
+        get() = _bricksNotFound
+
+    fun resetBricksNotFound() {
+        _bricksNotFound.value = listOf()
+    }
+
+    private val _addWhenInProgress = MutableLiveData(false)
+    val addWhenInProgress: LiveData<Boolean>
+        get() = _addWhenInProgress
+
+    fun handledAddWhenInProgress() {
+        _addWhenInProgress.value = false
+    }
+
+    enum class State {
+        NONE, IN_PROGRESS, WRONG_URL, NETWORK_ERROR, SUCCESS
+    }
+
+
     fun addInventory(setNumber: String, name: String) = viewModelScope.launch {
+        if (_result.value == State.IN_PROGRESS) {
+            _addWhenInProgress.value = true
+            return@launch
+        }
+        _result.value = State.IN_PROGRESS
+
         val url = "$urlPrefix$setNumber.xml"
         val response = try {
             requests.requestString(url)
         }
-        catch (error: VolleyError) {
-            _result.value = error.toString()
+        catch (error: ClientError) {
+            _result.value = State.WRONG_URL
+            return@launch
+        } catch (error: VolleyError) {
+            _result.value = State.NETWORK_ERROR
             return@launch
         }
 
@@ -40,27 +79,24 @@ class NewInventoryViewModel(application: Application) : AndroidViewModel(applica
         val inventory = parser.parse(response)
         inventory.inventory.name = name
 
-        try {
-            insert(inventory)
-        }
-        catch (error: Throwable) {
-            error.printStackTrace()
-            throw error
-        }
+        _result.value = State.SUCCESS
 
-        _result.value = "New project was created"
+        _bricksNotFound.value = insert(inventory)
     }
 
-    private suspend fun insert(inventory: InventoryWithParts) = withContext(Dispatchers.IO) {
+    private suspend fun insert(inventory: InventoryWithParts): List<String> = withContext(Dispatchers.IO) {
         val inventoryId = inventoryDao.insertInventory(inventory.inventory).toInt()
-
-        _result.postValue(inventoryId.toString())
+        val bricksNotFound = mutableListOf<String>()
+        val bricksNotFoundMutex = Mutex(false)
 
         val inventoryDeferred = inventory.parts.map { part ->
             async {
                 val color = brickListDao.getColorByCode(part.color.code)
                 val item = brickListDao.getItemByCode(part.item.code)
                 if (item == null || color == null) {
+                    bricksNotFoundMutex.withLock {
+                        bricksNotFound.add(part.item.code)
+                    }
                     return@async null
                 }
 
@@ -83,5 +119,6 @@ class NewInventoryViewModel(application: Application) : AndroidViewModel(applica
         }
         val inventoryParts = inventoryDeferred.mapNotNull { deferred -> deferred.await() }
         inventoryPartDao.insertInventoryParts(inventoryParts)
+        bricksNotFound
     }
 }
